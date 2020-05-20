4fadc607e96e879efd165773034318d8de2c2581
amarvila <amarvila@everis.com>
2020-05-20 18:10:34 +0200

    small code refactors

--- src/main/java/com/everis/d4i/tutorial/controllers/impl/FilmControllerImpl.java
+++ src/main/java/com/everis/d4i/tutorial/controllers/impl/FilmControllerImpl.java
@@ -9,10 +9,7 @@ import com.everis.d4i.tutorial.utils.constants.RestConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,11 +24,11 @@ public class FilmControllerImpl implements FilmController {
     @ResponseStatus(HttpStatus.OK)
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public NetflixResponse<List<FilmRest>> getFilmsFilteredStaticallyBy(
-            final Integer minimumDuration) {
+            @RequestParam(name = "minimumDuration", required = false) final Integer minimumDuration) {
         /* In this controller, only a parameter is collected: duration.
          The rest of examples are commented. Comment/uncomment to explore the other options.
          All the other returns call the functions with static values; should we want to collect from the user
-         the filtering values, the controller signature and method call should be changed.
+         the filtering values, the controller signature and method call should be changed. Feel free to do so ;)
          */
 
         return new NetflixResponse<>(CommonConstants.SUCCESS, String.valueOf(HttpStatus.OK), CommonConstants.OK,
