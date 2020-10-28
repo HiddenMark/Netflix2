b0889670159c4c0f3553a86474f39e116274f285
Aleix <aleix.garcia.gracia@everis.com>
2020-10-28 12:18:36 +0000

    MR - feature/lombokAndImproves into develop

--- src/main/java/com/everis/d4i/tutorial/persistence/CategoryRepository.java
+++ src/main/java/com/everis/d4i/tutorial/persistence/CategoryRepository.java
@@ -1,14 +1,14 @@
 package com.everis.d4i.tutorial.persistence;
 
-import com.everis.d4i.tutorial.persistence.entities.Category;
+import com.everis.d4i.tutorial.persistence.entity.CategoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface CategoryRepository extends JpaRepository<Category, Long> {
+public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {
 
-    Optional<Category> findByName(String name);
+    Optional<CategoryEntity> findByName(String name);
 
 }
